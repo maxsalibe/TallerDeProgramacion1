96077e15a2ded3722a6e16d5fc94737b01b4927e
maxsalibe <salibemaximiliano@gmail.com>
2022-09-08 21:18:10 -0300

    Tercer commit

--- TrabajoPractico3/src/ar/edu/ort/ejercicio03/Principal.java
+++ TrabajoPractico3/src/ar/edu/ort/ejercicio03/Principal.java
@@ -4,5 +4,6 @@ public class Principal {
     public static void main(String[] args) {
         BarajaInglesa baraja = new BarajaInglesa();
         baraja.mostrarBaraja();
+        System.out.println("Prueba Y");
     }
 }
